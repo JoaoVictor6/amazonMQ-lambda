2a69960095e2cb192929b1f4c8bf0a6278b84ecd
Joao Victor Dos Santos Candido <santosjoao.dev@gmail.com>
2025-09-14 18:48:13 -0300

    fix: adicionar variaveis de ambiente para localstack no script de teste

--- test.sh
+++ test.sh
@@ -10,6 +10,8 @@ sleep 30
 
 export AWS_ACCESS_KEY_ID=test
 export AWS_SECRET_ACCESS_KEY=test
+export AWS_DEFAULT_REGION=us-east-1
+export LOCALSTACK_HOST=localhost.localstack.cloud
 
 echo "Initializing Terraform..."
 cd terraform
